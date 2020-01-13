d0af7c482c6e5a13a6cba5065d2d496e26022e79
Ilya Dotsenko <vikontart@gmail.com>
2020-01-13 19:25:16 +0300

    fix

--- Distribution1c/Distribution.cs
+++ Distribution1c/Distribution.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -9,8 +10,7 @@ namespace Workplace1c.Distribution1c
 {
     class Distribution : INotifyPropertyChanged
     {
-        private string name = "", rootFolder = "";
-        // TODO сделать переменные окружения для заполнения файлов. serverLic, prefix, testCatalog, currentRelease mb.
+        private string name = "", rootFolder = "", prefix = "", testCatalog = "", licenseServerRelease = "";
 
         public int Id { get; set; }
 
@@ -20,19 +20,43 @@ namespace Workplace1c.Distribution1c
             set { name = value; OnPropertyChanged(nameof(Name)); }
         }
 
+        public string LicenseServerRelease
+        {
+            get => licenseServerRelease;
+            set { licenseServerRelease = value; OnPropertyChanged(nameof(LicenseServerRelease)); }
+        }
+
         public string RootFolder
         {
             get => rootFolder;
             set { rootFolder = value; OnPropertyChanged(nameof(RootFolder)); }
         }
 
+        public string Prefix
+        {
+            get => prefix;
+            set { prefix = value; OnPropertyChanged(nameof(Prefix)); }
+        }
+
+        public string TestCatalog
+        {
+            get => testCatalog;
+            set { testCatalog = value; OnPropertyChanged(nameof(TestCatalog)); }
+        }
+
+        [NotMapped]
+        public Release CurrentRelease { get; set; }
         public Platform Platform { get; set; }
 
         public ObservableCollection<Release> Releases { get; set; }
 
+        [NotMapped]        
+        public ObservableCollection<Release> PreviousReleases { get; set; }
+
         public Distribution()
         {
             Releases = new ObservableCollection<Release>();
+            PreviousReleases = new ObservableCollection<Release>();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
